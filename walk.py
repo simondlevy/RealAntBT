bfa3a84795d3227df069e648babae5a70e5f9754
simondlevy <simon.d.levy@gmail.com>
2022-07-18 20:45:08 +0100

    Walks for specified number of seconds

--- walk.py
+++ walk.py
@@ -38,63 +38,75 @@ def stand(ant):
     ant.set([45, 50, 45, -50, 45, 0, 45, 0])
 
 
+def sleepfor(sec):
+    sleep(sec)
+    return sec
+
+
 def step(ant):
 
+    t = 0
+
     ant.set([45, 50, 45, -50, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([30, 50, 45, -50, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([30, -20, 45, -50, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, -20, 45, -50, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 45, -20, 45, -50])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 30, -20, 45, -50])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 30, 50, 45, -50])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 45, 50, 45, -50])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 45, 50, 30, -50])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 45, 50, 30, 20])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 0, 45, 0, 45, 50, 45, 20])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 50, 45, 20, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 50, 30, 20, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
     ant.set([45, 50, 30, -50, 45, 0, 45, 0])
-    sleep(SLEEP_TIME)
+    t += sleepfor(SLEEP_TIME)
 
-
-def walk(ant):
-
-    while True:
-
-        try:
-            step(ant)
-
-        except KeyboardInterrupt:
-            break
+    return t
 
 
 def main():
 
-    # Allow user to specify a non-default com port
+    # Allow user to specify a non-default com port and runtime
     parser = OptionParser()
     parser.add_option('-p', '--port', dest='port',
                       help='com port, metavar="FILE',
                       default='/dev/ttyACM0')
+    parser.add_option('-t', '--time', dest='time',
+                      help='run time', default=5)
+
     (opts, _) = parser.parse_args()
 
     ant = RealAnt(opts.port)
 
     ant.connect()
 
-    walk(ant)
+    stand(ant)
+
+    total_time = 0
+
+    while total_time < opts.time:
+
+        try:
+            total_time += step(ant)
+
+        except KeyboardInterrupt:
+            break
+
     ant.disconnect()
 
 
