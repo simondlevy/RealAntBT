5a522fde4185c7a1e58c7abda2143b36515ba20b
simondlevy <simon.d.levy@gmail.com>
2022-07-19 20:30:19 +0100

    More bluetooth work

--- examples/bluetooth.py
+++ examples/bluetooth.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
 '''
-Subclasses BluetoothSocket to serve messages for running a servo motor on Raspberry Pi.
+Subclasses BluetoothSocket to serve messages for running the RealAnt
 
-Copyright 2018  Gunnar Bowman, Emily Boyes, Trip Calihan, Simon D. Levy, Shepherd Sims
+Copyright 2022  Matt Stock, Simon D. Levy
 
 MIT License
 '''
 
 from bluetooth_server import BluetoothServer
-import RPi.GPIO as GPIO
 
-class ServoServer(BluetoothServer):
+
+class RealAntServer(BluetoothServer):
 
     def __init__(self):
 
         BluetoothServer.__init__(self)
 
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setwarnings(False)
-        GPIO.setup(11,GPIO.OUT)
-
-        self.pwm = GPIO.PWM(11, 50)
-        self.pwm.start(5)
-
     def handleMessage(self, message):
 
-        # Convert [0,100] => [1,11]
-        duty = float(message)/10 + 1
-
-        # Don't know why we need this range check!
-        if duty >= 1 and duty <= 11:
-            self.pwm.ChangeDutyCycle(duty)
+        print(message)
 
 
 if __name__ == '__main__':
 
-    server = ServoServer()
+    server = RealAntServer()
 
     server.start()
