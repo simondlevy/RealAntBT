a4a9add6178582d500605dddc39aff9eb37c6711
simondlevy <simon.d.levy@gmail.com>
2022-07-19 23:46:43 +0100

    More Bluetooth client/server work

--- bluetooth/client.py
+++ bluetooth/client.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import socket
 
-server_address = "B8:27:EB:75:E6:45"
+server_address = 'B8:27:EB:75:E6:45'
 server_port = 1
 with socket.socket(socket.AF_BLUETOOTH,
                    socket.SOCK_STREAM,
                    socket.BTPROTO_RFCOMM) as c:
 
     c.connect((server_address, server_port))
-    c.send(b"desserts")
+    c.send(b'Move, ant!')
     print(c.recv(1024).decode())
 
