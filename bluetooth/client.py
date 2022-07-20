8d79f82c861e28ae3026819b1b64cb760cd3fb14
simondlevy <simon.d.levy@gmail.com>
2022-07-20 16:37:37 -0400

    Client sending numerical data

--- bluetooth/client.py
+++ bluetooth/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import socket
+import numpy as np
 
 server_address = 'B8:27:EB:75:E6:45'
 
@@ -12,6 +13,16 @@ with socket.socket(socket.AF_BLUETOOTH,
 
     c.connect((server_address, server_port))
 
+    angle = -90
+
     while True:
-        c.send(b'Move, ant!')
+
+        # c.send(b'Move, ant!')
+        #c.send(str(count).encode('utf8'))
+        c.send(bytearray([angle+90]))
+
+        angle += 1
+
+        if angle > 90:
+            angle = 0
 
