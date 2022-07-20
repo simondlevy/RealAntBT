241a7defed4e0cdd014e3f4abcd21f0c68380436
simondlevy <simon.d.levy@gmail.com>
2022-07-20 16:14:42 -0400

    Client has loop

--- bluetooth/client.py
+++ bluetooth/client.py
@@ -3,12 +3,15 @@
 import socket
 
 server_address = 'B8:27:EB:75:E6:45'
+
 server_port = 1
+
 with socket.socket(socket.AF_BLUETOOTH,
                    socket.SOCK_STREAM,
                    socket.BTPROTO_RFCOMM) as c:
 
     c.connect((server_address, server_port))
-    c.send(b'Move, ant!')
-    print(c.recv(1024).decode())
+
+    while True:
+        c.send(b'Move, ant!')
 
