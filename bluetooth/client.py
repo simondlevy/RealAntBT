612ce69c2ae0569e312dbd3c17c48019e60bb5df
simondlevy <simon.d.levy@gmail.com>
2022-07-20 16:58:29 -0400

    Begin walk.py bluetooth version

--- bluetooth/client.py
+++ bluetooth/client.py
@@ -9,13 +9,13 @@ server_port = 1
 
 with socket.socket(socket.AF_BLUETOOTH,
                    socket.SOCK_STREAM,
-                   socket.BTPROTO_RFCOMM) as c:
+                   socket.BTPROTO_RFCOMM) as conn:
 
-    c.connect((server_address, server_port))
+    conn.connect((server_address, server_port))
 
     while True:
 
         angles = [int(v) for v in np.random.random(8) * 180 - 90]
 
         # We add 90 because we can't encode negative values
-        c.send(bytearray([a+90 for a in angles]))
+        conn.send(bytearray([a+90 for a in angles]))
---
+++ bluetooth/walk.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+
+import time
+from optparse import OptionParser
+
+def stand(client, sleep_time):
+
+    angles = [
+            [45, None, 45, None, 45, None, 45, None],
+            [30, None, 45, None, 45, None, 45, None],
+            [30, 50, 45, None, 45, None, 45, None],
+            [45, 50, 45, None, 45, None, 45, None],
+            [45, 50, 30, None, 45, None, 45, None],
+            [45, 50, 30, -50, 45, None, 45, None],
+            [45, 50, 45, -50, 45, None, 45, None],
+            [45, 50, 45, -50, 30, None, 45, None],
+            [45, 50, 45, -50, 30, 0, 45, None],
+            [45, 50, 45, -50, 45, 0, 45, None],
+            [45, 50, 45, -50, 45, 0, 30, None],
+            [45, 50, 45, -50, 45, 0, 30, 0],
+            [45, 50, 45, -50, 45, 0, 45, 0]
+            ]
+
+    for angle in angles:
+        ant.set(angle)
+        time.sleep(sleep_time)
+
+
+def step(ant, sleep_time, max_time):
+    '''
+    Returns total time taken on this step, rounded to sleep_time
+    '''
+
+    angles = [
+            [45, 50, 45, -50, 45, 0, 45, 0],
+            [30, 50, 45, -50, 45, 0, 45, 0],
+            [30, -20, 45, -50, 45, 0, 45, 0],
+            [45, -20, 45, -50, 45, 0, 45, 0],
+            [45, 0, 45, 0, 45, -20, 45, -50],
+            [45, 0, 45, 0, 30, -20, 45, -50],
+            [45, 0, 45, 0, 30, 50, 45, -50],
+            [45, 0, 45, 0, 45, 50, 45, -50],
+            [45, 0, 45, 0, 45, 50, 30, -50],
+            [45, 0, 45, 0, 45, 50, 30, 20],
+            [45, 0, 45, 0, 45, 50, 45, 20],
+            [45, 50, 45, 20, 45, 0, 45, 0],
+            [45, 50, 30, 20, 45, 0, 45, 0],
+            [45, 50, 30, -50, 45, 0, 45, 0]
+            ]
+
+    for (count, angle) in enumerate(angles):
+        ant.set(angle)
+        time.sleep(sleep_time)
+        if count*sleep_time >= max_time:
+            break
+
+    return sleep_time * len(angles)
+
+
+def main():
+
+    # Allow user to specify a non-default com port and runtime
+    parser = OptionParser()
+    parser.add_option('-p', '--port', dest='port',
+                      help='com port, metavar="FILE',
+                      default='/dev/ttyACM0')
+    parser.add_option('-t', '--time', dest='time', help='run time',
+                      type='float', default=5)
+    parser.add_option('-s', '--sleep', dest='sleep', help='sleep time',
+                      type='float', default=0.4)
+
+    (opts, _) = parser.parse_args()
+
+    start = time.time()
+
+    time_left = opts.time
+
+    while True:
+
+        try:
+            time_left -= step(ant, opts.sleep, time_left)
+
+            if time_left <= 0:
+                break
+
+        except KeyboardInterrupt:
+            break
+
+    print(time.time()-start)
+
+
+if __name__ == "__main__":
+    main()
