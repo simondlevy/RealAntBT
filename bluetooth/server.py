1d1665eefd904278cc9819522c1f95d7e08b40a3
simondlevy <simon.d.levy@gmail.com>
2022-07-20 21:07:04 +0100

    Cleanup

--- bluetooth/server.py
+++ bluetooth/server.py
@@ -3,30 +3,38 @@
 import socket
 import os
 
+ADDRESS = 'B8:27:EB:75:E6:45'
+PORT    = 1
+MSGSIZE = 1024
+
 # Enable bluetooth
 os.system('sudo hciconfig hci0 piscan')
 
-server_address = 'B8:27:EB:75:E6:45'
-
-server_port = 1
-backlog = 1
-size = 1024
 while True:
+
     with socket.socket(socket.AF_BLUETOOTH,
                        socket.SOCK_STREAM,
                        socket.BTPROTO_RFCOMM) as s:
-        s.bind((server_address, server_port))
-        s.listen(backlog)
+
+        s.bind((ADDRESS, PORT))
+
+        s.listen(1) # support only one client
+
         print('Waiting for connection...')
         client, address = s.accept()
+
         print(f'Connection from {address}')
+
         while True:
+
             try:
-                data = client.recv(size)
+                data = client.recv(MSGSIZE)
                 if data:
                     print(data)
-                    client.send(data)
+
             except ConnectionResetError:
                 print('Client disconnected')
                 break
 
+            except KeyboardInterrupt:
+                break
